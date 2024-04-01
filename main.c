7117ad6e39c9e5b7bcdf33240e9f6b66fb29c962
ChronoX27 <lu.hauber@web.de>
2024-04-01 16:00:03 +0200

    clean menu

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-
-int print_menu(){
+int print_menu()
+{
     printf("Choose an option: \n");
     printf(" 1. List entries\n");
     printf(" 2. Show an entry\n");
     printf(" 3. Create a new entry\n");
-    printf(" 4. Delete an entry\n");
-    printf(" 5. Quit\n");
+    printf(" 4. Change entry\n");
+    printf(" 5. Delete an entry\n");
+    printf(" 6. Quit\n");
 
     printf(" > ");
     int option;
@@ -15,32 +17,70 @@ int print_menu(){
     return option;
 }
 
-void list_entries() {}
-void show_entry() {}
-void create_entry() {}
-void delete_entry() {}
+void clear() {
+    printf("\e[1;1H\e[2J");
+}
 
-int main() {
+void list_entries()
+{
+    clear();
+    printf("=== LIST ENTRIES ===\n\n");
+}
+
+void show_entry()
+{
+    clear();
+    printf("=== SHOW ENTRY ===\n\n");
+}
+
+void create_entry()
+{
+    clear();
+    printf("=== CREATE NEW ENTRY ===\n\n");
+}
+
+void change_entry()
+{
+    clear();
+    printf("=== CHANGE ENTRY ===\n\n");
+}
+
+void delete_entry()
+{
+    clear();
+    printf("=== DELETE ENTRY ===\n\n");
+}
+
+int main()
+{
     int option = 0;
-    option = print_menu();
-
-    switch (option)
+    bool quit = false;
+    while (!quit)
     {
-    case 1:
-        list_entries();
-        break;
-    case 2:
-        show_entry();
-        break;
-    case 3:
-        create_entry();
-        break;
-    case 4:
-        delete_entry();
-        break;
-    default:
-        printf("Quiting...\n");
-        break;
+        option = print_menu();
+
+        switch (option)
+        {
+        case 1:
+            list_entries();
+            break;
+        case 2:
+            show_entry();
+            break;
+        case 3:
+            create_entry();
+            break;
+        case 4:
+            change_entry();
+            break;
+        case 5:
+            delete_entry();
+            break;
+        default:
+            printf("Quiting...\n");
+            quit = true;
+            break;
+        }
     }
     return 0;
 }
